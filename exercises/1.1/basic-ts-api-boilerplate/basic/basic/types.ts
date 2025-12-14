f73594392484458a06f04c291b49e6a0d93c602d
Kajewski <adrian.kajewski@student.vinci.be>
2025-12-14 17:52:23 +0100

    added drinks

--- exercises/1.1/basic-ts-api-boilerplate/basic/basic/types.ts
+++ exercises/1.1/basic-ts-api-boilerplate/basic/basic/types.ts
@@ -12,3 +12,14 @@ interface PizzaToUpdate {
 type NewPizza = Omit<Pizza, "id">;
 
 export type { Pizza, NewPizza, PizzaToUpdate };
+
+
+interface Drink {
+  id:number;
+  title: string;
+  image: string;
+  volume: number;
+  price: number;
+}
+
+export type {Drink};
